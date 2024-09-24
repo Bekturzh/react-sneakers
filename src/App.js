9b6e310c4e3904ef77c7d6dc9bcfca501ca0314f
Bektur <101509717+Bekturzh@users.noreply.github.com>
2024-09-24 12:44:14 +0600

    clone

---
+++ src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+          <div className="headerLeft">
+            <svg />
+              <div className="headerInfo">
+                <h3>React Sneakers</h3>
+                <p>Магазин лучшмх кроссовок</p>
+              </div>
+            </div>
+            <ul className="headerRight">
+              <li>
+                <svg/>
+                <span>1205 руб.</span>
+                <svg/>
+              </li>
+            </ul>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
+    </div>  
+  );
+}
+
+export default App;
