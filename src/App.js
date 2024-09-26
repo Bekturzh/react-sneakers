54200dd72bf85ccb525006f41985d72219e03521
Bektur <101509717+Bekturzh@users.noreply.github.com>
2024-09-26 16:00:52 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay"> 
+        <div className="drawer"> 
+          <h2>Корзина</h2>
+
+          <div className="cartItem d-flex align-center">
+            <img className="mr-20"  width={70} height={70} src="/img/sneaker/sneaker-1.jpg"/>
+            <div className="mr-20">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-plus.svg"/>
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
           <div className="d-flex align-between">
             <img width={40}  height={40} src="/img/logo.png"/>
@@ -21,10 +36,19 @@ function App() {
             </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" />
+            <input placeholder="Поиск..."/>
+          </div>
+        </div>
 
         <div className="d-flex">
           <div className="card ">
+          <div className="favorite">
+            <img src="/img/heart-disable.svg"/>
+          </div>
             <img width={133} height={112} src="/img/sneaker/sneaker-1.jpg"/>
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
               <div className="d-flex justify-between align-center">
